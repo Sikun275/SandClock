dc356edb7879a9e0ae13609d42b14bfda8c3be4e
Sikun275 <sikun275@outlook.com>
2025-03-20 15:18:00 -0400

    Initial Commit

---
+++ SandClock/SandClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandClockApp.swift
+//  SandClock
+//
+//  Created by Sikun Chen on 2025-03-20.
+//
+
+import SwiftUI
+
+@main
+struct SandClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SandClockTests/SandClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  SandClockTests.swift
+//  SandClockTests
+//
+//  Created by Sikun Chen on 2025-03-20.
+//
+
+import Testing
+@testable import SandClock
+
+struct SandClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
